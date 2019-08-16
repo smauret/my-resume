b5297239864b2242569d6c4500ba45eb087a8d94
Sarah MAURET <sarah.mauret@talan.com>
2019-08-16 16:03:53 +0200

    Updates

---
+++ precache-manifest.8ca6653ee16be413e58569d4e3480e43.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ebff6f78de307f4bfbf0954241bd43",
+    "url": "/smauret.github.io/index.html"
+  },
+  {
+    "revision": "c86f09216e2999f75a97",
+    "url": "/smauret.github.io/static/js/2.537f088b.chunk.js"
+  },
+  {
+    "revision": "c66f0d6f4e73e6a33cd4",
+    "url": "/smauret.github.io/static/js/main.d3a818a2.chunk.js"
+  },
+  {
+    "revision": "0a5ed7242ed7cde5ea7d",
+    "url": "/smauret.github.io/static/js/runtime~main.ba2eab14.js"
+  }
+]);
