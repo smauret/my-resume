63561db900ae25e3c3123ccbdb396493513e91d8
Sarah MAURET <sarah.mauret@talan.com>
2019-08-31 19:23:46 +0200

    hide menu smDown

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,27 +1,25 @@
 import React from 'react'
 import {NavHashLink as NavLink} from "react-router-hash-link";
-import {Grid} from "@material-ui/core";
+import {Grid, Hidden} from "@material-ui/core";
 import {grey} from '@material-ui/core/colors';
-
-/*function removeHash () {
-  window.history.pushState("", document.title, window.location.pathname
-    + window.location.search);
-}*/
-
-/*<button onClick={removeHash}>Hash</button>*/
 
 export const Menu = ({list, visibleComponent}) => {
   return (
-    <Grid container item md={3} style={{position: 'sticky', top: '0', height: 'fit-content', marginTop:'20px'}}>
-        {list.map(item =>
+    <Grid container item md={3} style={{position: 'sticky', top: '0', height: 'fit-content', marginTop: '20px'}}>
+      {list.map(item =>
+        <Hidden smDown>
           <Grid container item key={item} xs={12} style={{padding: '10px 20px', justifyContent: 'flex-end'}}>
-            <NavLink isActive={()=>{ return window.location.pathname + window.location.hash === `/#${item}` || visibleComponent===item}}
-                     smooth to={`/#${item}`} activeStyle={{fontWeight: "bold", fontSize:'36px', borderBottom: '3px solid black'}}
-                     style={{textDecoration: 'none', fontSize:'31px',fontFamily:'system-ui', color: grey['800']}}>
+            <NavLink isActive={() => {
+              return window.location.pathname + window.location.hash === `/#${item}` || visibleComponent === item
+            }}
+                     smooth to={`/#${item}`}
+                     activeStyle={{fontWeight: "bold", fontSize: '36px', borderBottom: '3px solid black'}}
+                     style={{textDecoration: 'none', fontSize: '31px', fontFamily: 'system-ui', color: grey['800']}}>
               {item}
             </NavLink>
           </Grid>
-        )}
+        </Hidden>
+      )}
     </Grid>
   )
 };
