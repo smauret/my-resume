526c4ab5b4db7bee8c7e41f4d75f205125615610
Sarah MAURET <sarah.mauret@talan.com>
2019-10-26 21:52:05 +0200

    Updates

--- precache-manifest.f1ea5a9af80b9573dac29a10829bf6ee.js
+++ precache-manifest.f1ea5a9af80b9573dac29a10829bf6ee.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "705fe3633df5ce7964ba4d0807ef77b0",
+    "revision": "a6bced03d65ffcedb487f0848e9a4f9c",
     "url": "/my-resume/index.html"
   },
   {
-    "revision": "558f9750c26d842276e2",
-    "url": "/my-resume/static/js/2.22155eb8.chunk.js"
+    "revision": "beeca393c2e68ccbee7f",
+    "url": "/my-resume/static/js/2.5648d481.chunk.js"
   },
   {
-    "revision": "08cb8040a823db471ae8",
-    "url": "/my-resume/static/js/main.17aa018d.chunk.js"
+    "revision": "c3f4bf87ab333dafa8b2",
+    "url": "/my-resume/static/js/main.df16fe24.chunk.js"
   },
   {
     "revision": "2f7b259ab1933d650523",
@@ -20,12 +20,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-resume/static/media/asseth-logo.545ad011.jpg"
   },
   {
+    "revision": "8bc0b69577967689c1b5686d44586117",
+    "url": "/my-resume/static/media/beer-pong-project.8bc0b695.png"
+  },
+  {
     "revision": "eb98d0b7a737e3e30b4ef94a56d35363",
     "url": "/my-resume/static/media/dev-team-share2gether.eb98d0b7.jpg"
   },
   {
     "revision": "f279b77b97de0db47c0f0d43cd80e949",
     "url": "/my-resume/static/media/ethereum-2.f279b77b.png"
+  },
+  {
+    "revision": "c80a276026b6ca4cb2666a3fe19a4bda",
+    "url": "/my-resume/static/media/pedestrian-intention-project.c80a2760.png"
+  },
+  {
+    "revision": "9b76cfbc105c08ac78e9ba67784da9a9",
+    "url": "/my-resume/static/media/personal-project.9b76cfbc.png"
   },
   {
     "revision": "2bf4c2d57515e4f80ab79a0eb400ab66",
